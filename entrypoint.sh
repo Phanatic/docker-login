fc2846aba0ffab2779b0b9aaedd1fcd5fbe3094c
Nick Van Wiggeren <nickvanw@users.noreply.github.com>
2018-09-18 10:34:25 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 mkdir -p $HOME/.docker
 echo $HOME
-DOCKER_HOST="${DOCKER_HOST:=https://index.docker.io/v1}"
+DOCKER_HOST="${DOCKER_HOST:=https://index.docker.io/v1/}"
 printf '{"auths":{"%s":{"auth":"%s"}}}\n' "$DOCKER_HOST" "$DOCKER_AUTH" >> $HOME/.docker/config.json
 cat $HOME/.docker/config.json

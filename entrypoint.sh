4b7d6af0f9d2efa19cfb065c069f14928d07ce88
Nick Van Wiggeren <nickvanw@users.noreply.github.com>
2018-09-18 10:39:50 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 mkdir -p $HOME/.docker
-echo $HOME
+if [ -z "$DOCKER_AUTH" ]; then
+    echo "I need the \$DOCKER_AUTH variable to be set for this to work!\n"
+    exit 1
+fi
 DOCKER_HOST="${DOCKER_HOST:=https://index.docker.io/v1/}"
 printf '{"auths":{"%s":{"auth":"%s"}}}\n' "$DOCKER_HOST" "$DOCKER_AUTH" >> $HOME/.docker/config.json
-cat $HOME/.docker/config.json

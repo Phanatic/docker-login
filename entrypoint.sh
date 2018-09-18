9b41e6b1b34f99de7bceafffbf4626a4d91568d8
Nick Van Wiggeren <nickvanw@users.noreply.github.com>
2018-09-18 10:02:37 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir -p $HOME/.docker
+echo $HOME
 DOCKER_HOST="${DOCKER_HOST:=https://index.docker.io/v1}"
 printf '{"ServerURL":"%s","Username":"%s","Secret":"%s"}\n' "$DOCKER_HOST" "$DOCKER_USERNAME" "$DOCKER_SECRET" >> $HOME/.docker/config.json
 cat $HOME/.docker/config.json
